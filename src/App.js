4ce28a9f07f49c452964e11fc5f25a673d505f96
Fmajid29 <faizanmajid11@gmail.com>
2023-02-26 18:15:14 +0500

    resolved the git issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-// import Home from "./Pages/Home";
-//import SelectOrder from "./Pages/SelectOrder";
-import Cart from "./Pages/Cart";
-
+import Home from "./Pages/Home";
+// import SelectOrder from "./Pages/SelectOrder";
+// import Cart from "./Pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-
-      <div>
-      <Navbar />
-      {/* <Home /> */}
-      {/* <SelectOrder/> */}
-      <Cart/>
-       </div>
-
-   
-
-
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/footer" element={<Footer />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
 export default App;
