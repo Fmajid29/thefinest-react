d3a8ba6db105f1d19df1ea2a2a2b2416c24548e4
Fmajid29 <faizanmajid11@gmail.com>
2023-02-26 16:49:43 +0500

    started working on routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Navbar from "./Components/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./Pages/Home";
-import SelectOrder from "./Pages/SelectOrder";
-import Cart from "./Pages/Cart";
+// import SelectOrder from "./Pages/SelectOrder";
+// import Cart from "./Pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
-      {/* <SelectOrder /> */}
-      {/* <Cart/> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/footer" element={<Footer />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
