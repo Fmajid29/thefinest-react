8b78bf7236e3ca87a5b466add20008898dbec540
Fmajid29 <faizanmajid11@gmail.com>
2023-02-28 00:41:16 +0500

    fixed the errors with options

--- src/Components/Options/Options.jsx
+++ src/Components/Options/Options.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-import {FaPizzaSlice} from 'react-icons/fa';
-import {GiHamburger} from 'react-icons/gi';
-import {MdFastfood} from 'react-icons/md';
-import {CiCoffeeCup} from 'react-icons/ci';
-import {FaBreadSlice} from 'react-icons/fa';
-
+import { FaPizzaSlice } from "react-icons/fa";
+import { GiHamburger } from "react-icons/gi";
+import { MdFastfood } from "react-icons/md";
+import { CiCoffeeCup } from "react-icons/ci";
+import { FaBreadSlice } from "react-icons/fa";
+// import { Link } from "react-router-dom";
 
 import "./Options.css";
 const Options = () => {
   return (
     <div className="OptionBar">
-      
-
-        
-         <ul className="MainBar">
-          <li><a href="#"><FaPizzaSlice className="OptionIcon"/><span>PIZZA</span></a></li>
-          <li><a href="#"><GiHamburger className="OptionIcon"/><span>BURGER</span></a></li>
-          <li><a href="#"><FaBreadSlice className="OptionIcon"/><span>SANDWHICH</span></a></li>
-          <li><a href="#"><MdFastfood className="OptionIcon"/><span>OTHERS</span></a></li>
-          <li><a href="#"><CiCoffeeCup id="LOptionIcon"/><span>DRINKS</span></a></li>
-         </ul>
-        
-       
-      
+      <ul className="MainBar">
+        <li>
+          <FaPizzaSlice className="OptionIcon" />
+          <span>PIZZA</span>
+        </li>
+        <li>
+          <GiHamburger className="OptionIcon" />
+          <span>BURGER</span>
+        </li>
+        <li>
+          <FaBreadSlice className="OptionIcon" />
+          <span>SANDWHICH</span>
+        </li>
+        <li>
+          <MdFastfood className="OptionIcon" />
+          <span>OTHERS</span>
+        </li>
+        <li>
+          <CiCoffeeCup id="LOptionIcon" />
+          <span>DRINKS</span>
+        </li>
+      </ul>
     </div>
   );
 };
