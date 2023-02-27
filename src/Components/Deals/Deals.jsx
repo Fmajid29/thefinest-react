828aff7cd4c19ed9148eaf36c5c09e28a86904a9
Shalina <shalinakhan936@gmail.com>
2023-02-28 01:34:58 +0500

    Deals Complete

--- src/Components/Deals/Deals.jsx
+++ src/Components/Deals/Deals.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
-
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import img from './Deal.jpg';
+import './Deals.css';
 const Deals = () => {
-  return (
-    <div>Deals</div>
-  )
+    return (
+
+       
+            <div className='DealCard'>
+                <img src={img} />
+                <div className='section2'>
+                  <div>
+                        <h3>Rs 1500</h3>
+                    </div>
+                    <div className='details' id='DealDetail'>
+                        <p>2 Burgers</p>
+                        <p>Regular Fries</p>
+                        <p>2 wings</p>
+                    </div>
+                    <div className='radiobtn'>
+                    
+                        <Form>
+                        
+                            {['radio'].map((type) => (
+                                <>
+                                
+                                <div key={`inline-${type}`} className="mb-3">
+                                    <Form.Check
+                                        inline
+                                        label="Sprite"
+                                        name="group1"
+                                        type={type}
+                                        id={`inline-${type}-1`}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label="Coke"
+                                        name="group1"
+                                        type={type}
+                                        id={`inline-${type}-2`}
+                                    />
+
+                                </div>
+                                </>
+                               
+                            ))}
+                        </Form>
+                    </div>
+                    
+                </div>
+
+                <Button>Add To Cart</Button>
+            </div>
+        
+
+
+    )
 }
 
 export default Deals
--- src/Pages/DealsPage.jsx
+++ src/Pages/DealsPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Deals from '../Components/Deals/Deals';
+import '../Components/Deals/Deals.css';
 const DealsPage = () => {
   return (
-    <div>DealsPage</div>
+    <div className='MainSec'>
+      <Deals/>
+      <Deals/>
+      <Deals/>
+    </div>
   )
 }
 
